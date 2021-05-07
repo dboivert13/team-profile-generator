159e0adda2d99a8a3af645c364c98edc8d4e092d
Your Name <you@example.com>
2021-05-06 21:45:41 -0400

    index.js package update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
 const Employee = require("./lib/employee");
 const Engineer = require("./lib/engineer");
