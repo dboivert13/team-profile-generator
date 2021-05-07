955429d93d91a7aec12f26c9cfd6ee2ae4a32854
Your Name <you@example.com>
2021-05-06 21:58:00 -0400

    inquirer questions

--- index.js
+++ index.js
@@ -7,4 +7,61 @@ const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
 
-const fs = require("fs");
+
+function runInquirer() {
+    const promptArray = [{
+        type: "input",
+        message: "What is your name?",
+        name: "name"
+    }, {
+        type: "input",
+        message: "What is your ID?",
+        name: "id"
+    }, {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+    }, {
+        type: "list",
+        message: "What is your title",
+        choices: ["Manager", "Engineer", "Intern"],
+        name: "title"
+    }];
+
+    return inquirer
+        .prompt(promptArray);
+}
+
+function runInquirerManager() {
+    const promptArray = [{
+        type: "input",
+        message: "What is your office number?",
+        name: "officeNumber"
+    }];
+
+    return inquirer
+        .prompt(promptArray);
+}
+
+function runInquirerEngineer() {
+    const promptArray = [{
+        type: "input",
+        message: "What is your github?",
+        name: "github"
+    }];
+
+    return inquirer
+        .prompt(promptArray);
+}
+
+function runInquirerIntern() {
+    const promptArray = [{
+        type: "input",
+        message: "What school do you attend?",
+        name: "school"
+    }];
+
+    return inquirer
+        .prompt(promptArray);
+}
+
